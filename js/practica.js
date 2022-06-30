34cb391a98fde3402af0ea5dc76f873a2e0902f4
thomas jerez <tjerez@outlook.com>
2022-06-30 17:30:41 -0300

    agregamos ejemplo de prompt y ejercicio 5

---
+++ js/practica.js
@@ -0,0 +1,32 @@
+//alert("un mensaje");
+
+document.write("<h1>Hello World</h1>");
+
+document.write("Resultado:" + (3 + 5));
+
+
+document.write("<br>holaComoEstas");
+
+
+
+
+/*para crear una variable tengo que seleccionar entre (let-const)
+*/
+
+let nombre = prompt("Ingrese su nombre");
+
+console.log(nombre)
+
+document.write("<br> Hola " + nombre );
+
+document.write("<p>Hola" + nombre + "</p>");
+
+
+
+
+
+
+
+
+
+
