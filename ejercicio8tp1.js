9e872e2c2ca232a811081b63c8d6eb1a8e841e8e
thomas jerez <tjerez@outlook.com>
2022-07-01 12:46:20 -0300

    cambios

---
+++ ejercicio8tp1.js
@@ -0,0 +1,11 @@
+let numero1 = parseInt( prompt("Ingrese el primer numero"))
+
+
+console.log(numero1);
+
+
+if (numero1 / 2 === 2 ){
+    document.write("Es divisible por 2")
+}else{
+    document.write("No es divisible por 2")
+}
