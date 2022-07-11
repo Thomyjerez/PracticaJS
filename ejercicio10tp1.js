821f0405a3e1bea48fd78cf1902f12ec192cfd06
thomas jerez <tjerez@outlook.com>
2022-07-11 13:12:36 -0300

    nuevas correciones

--- ejercicio10tp1.js
+++
@@ -1,9 +0,0 @@
-let numero1 = parseInt(prompt("Ingrese el primer numero"));
-
-console.log(numero1)
-
-if (numero1 % 2 === 0 || numero1 % 3 === 3 || numero1 % 5 === 5 || numero1 % 7 === 7 ) {
-    document.write("Es divisible por 2, 3, 5 y 7");
-}else{
-    document.write ("No es divisible por 2, 3, 5 y 7");
-}
--- ejercicio5tp1.js
+++
@@ -1,7 +0,0 @@
-let numero1 = parseInt(prompt("Ingrese el primer numero"))
-let numero2= parseInt(prompt("Ingrese el segundo numero"))
-
-//console.log(numero1);
-//console.log(parseInt("10"));
-
-document.write("El resultado de la suma es: " + (numero1 + numero2));
