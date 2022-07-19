0a721ff63c775325e7c63eeb30144d883074e236
thomas jerez <tjerez@outlook.com>
2022-07-19 18:07:10 -0300

    hice correcciones en ejercicios 10 y 11

--- js/ejercicios/ejercicio10tp1.js
+++ js/ejercicios/ejercicio10tp1.js
@@ -2,7 +2,7 @@ let numero1 = parseInt(prompt("Ingrese el primer numero"));
 
 console.log(numero1)
 
-if (numero1 % 2 === 2 || numero1 % 3 === 3 || numero1 % 5 === 5 || numero1 % 7 === 7 ) {
+if (numero1 % 2 === 0 || numero1 % 3 === 0 || numero1 % 5 === 0 || numero1 % 7 === 0 ) {
     document.write("Es divisible por 2, 3, 5 y 7");
 }else{
     document.write ("No es divisible por 2, 3, 5 y 7");
